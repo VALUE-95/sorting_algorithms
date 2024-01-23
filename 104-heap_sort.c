e354eff0955793b290fa09dad69fb49087575608
OCHUBA VICTOR OGECHUKWU <vochuba95@gmail.com>
2024-01-23 15:06:23 -0800

    second trial

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,78 +1,60 @@
 #include "sort.h"
 
 /**
- * merge_sort - sorts an array with the Merge Sort algorithm
+ * heap_sort - sorts an array following the Heap sort algorithm
  * @array: array of ints to sort
- * @size: size of the array
+ * @size: size of the array to sort
  */
-void merge_sort(int *array, size_t size)
+void heap_sort(int *array, size_t size)
 {
-	int *arr;
+	int i;
+	int tmp;
 
-	if (!array || size < 2)
+	if (size < 2)
 		return;
 
-	arr = malloc(sizeof(int) * size);
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, (size_t)i, size);
 
-	merge_recursion(arr, array, 0, size);
-	free(arr);
-}
-
-/**
- * merge_recursion - recursive function that merge sorts an array
- * @arr: copy array
- * @array: array to merge sort
- * @left: index of the left element
- * @right: index of the right element
- */
-void merge_recursion(int *arr, int *array, size_t left, size_t right)
-{
-	size_t middle;
-
-	if (right - left > 1)
+	for (i = size - 1; i >= 0; i--)
 	{
-		middle = (right - left) / 2 + left;
-		merge_recursion(arr, array, left, middle);
-		merge_recursion(arr, array, middle, right);
-		merge_subarray(arr, array, left, middle, right);
+		tmp = array[i];
+		array[i] = array[0];
+		array[0] = tmp;
+		if (i != 0)
+			print_array(array, size);
+		heapify(array, (size_t)i, 0, size);
 	}
 }
 
 /**
- * merge_subarray - merges subarrays
- * @arr: copy array
- * @array: array to merge
- * @left: index of the left element
- * @middle: index of the middle element
- * @right: index of the right element
+ * heapify - turns an array in a heap tree
+ * @array: array to turn into heap
+ * @s: size of the subtree
+ * @root: index of the subtree in the heap
+ * @size: size of the whole array
  */
-void merge_subarray(int *arr, int *array, size_t left,
-		size_t middle, size_t right)
+void heapify(int *array, size_t s, size_t root, size_t size)
 {
-	size_t i, j, k = 0;
+	size_t max, left, right;
+	int tmp;
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(array + left, middle  - left);
-	printf("[right]: ");
-	print_array(array + middle, right - middle);
+	max = root;
+	left = (root * 2) + 1;
+	right = (root * 2) + 2;
 
-	for (i = left, j = middle; i < middle && j < right; k++)
+	if (left < s && array[left] > array[max])
+		max = left;
+
+	if (right < s && array[right] > array[max])
+		max = right;
+
+	if (max != root)
 	{
-		if (array[i] < array[j])
-			arr[k] = array[i++];
-		else
-			arr[k] = array[j++];
+		tmp = array[root];
+		array[root] = array[max];
+		array[max] = tmp;
+		print_array(array, size);
+		heapify(array, s, max, size);
 	}
-
-	while (i < middle)
-		arr[k++] = array[i++];
-	while (j < right)
-		arr[k++] = array[j++];
-
-	for (k = left, i = 0; k < right; k++)
-		array[k] = arr[i++];
-
-	printf("[Done]: ");
-	print_array(array + left, right - left);
 }
